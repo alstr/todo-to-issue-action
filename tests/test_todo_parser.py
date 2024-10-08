26ed78e3f4de621d970ccb3a8c68ca018cf3d640
Miguel Palhas <mpalhas@gmail.com>
2024-10-08 11:55:07 +0100

    code review

--- tests/test_todo_parser.py
+++ tests/test_todo_parser.py
@@ -100,6 +100,9 @@ class NewIssueTests(unittest.TestCase):
     def test_liquid_issues(self):
         self.assertEqual(count_issues_for_file_type(self.raw_issues, 'liquid'), 3)
 
+    def test_liquid_issues(self):
+        self.assertEqual(count_issues_for_file_type(self.raw_issues, 'solidity'), 3)
+
 class ClosedIssueTests(unittest.TestCase):
     # Check for removed TODOs across the files specified.
     def setUp(self):
